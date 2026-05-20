53aa330752d02e9da56c143edad928248ba3802a
Cristopher Vanegas <cristopher.platzi@gmail.com>
2026-05-20 17:21:17 -0500

    [ADD][CV]: Added keycloak as a log in system (IAM) with Open-ID Conecct (OIDC)

--- react_search_hooks-main/src/App.js
+++ react_search_hooks-main/src/App.js
@@ -1,12 +1,74 @@
-import './App.css';
-import SearchComponent from './components/SearchComponent';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchComponent from "./components/SearchComponent";
+
+import Keycloak from "keycloak-js";
+
+// Configuracion Del Keycloak
+const keycloakOptions = {
+  url: "http://localhost:8080",
+  realm: "REINO_ITSMRJKALI21",
+  clientId: "react-search-app-client",
+};
 
 function App() {
+  // Inicializo El Keycloak
+  const [keycloak, setKeycloak] = useState(null);
+
+  useEffect(() => {
+    const initKeyclock = async () => {
+      const keycloakInstance = new Keycloak(keycloakOptions);
+      try {
+        await keycloakInstance.init({ onLoad: "login-required" });
+        setKeycloak(keycloakInstance);
+        if (keycloakInstance.authenticated) {
+          console.log(keycloakInstance);
+        }
+      } catch (error) {
+        console.log(`Error ${error}`);
+      }
+    };
+    initKeyclock();
+  }, []);
+
+  // Logica Para El Boton De Logout
+  const handleLogout = () => {
+    if (keycloak) {
+      keycloak.logout();
+    }
+  };
+
   return (
-    <div className="container-fluid">
-     <h2 className='text-center'>React Search</h2>
-      <SearchComponent />     
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <h2 className="text-center">React Search</h2>
+          </a>
+          {/* Si no se autentica no muestra el boton de logout */}
+          {keycloak && keycloak.authenticated ? (
+            <button
+              className="btn btn-outline-danger"
+              type="button"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          ) : null}
+        </div>
+      </nav>
+      {/* 
+      Si No Hay Loggueo Entonces No Hay Nada, 
+      Si Se Logguea Entonces Muestra La Pantalla De Busqueda */}
+      <div className="container-fluid">
+        {keycloak && keycloak.authenticated ? (
+          <>
+            <h2 className="text-center">Search</h2>
+            <SearchComponent />
+          </>
+        ) : null}
+      </div>
+    </>
   );
 }
 
